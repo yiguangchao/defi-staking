1aa4f303a3eabfb71ed942b84e48321f7bfe5fb5
GuangchaoYi <1241354012@qq.com>
2026-01-02 23:57:04 +0800

    feat: #1 Write core vault contracts (Vault. sol)

--- src/Vault.sol
+++ src/Vault.sol
@@ -7,13 +7,13 @@ import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 // Vault the contract itself is also an ERC20 token (representing shares)
 contract Vault is ERC20 {
     // 1. state variable
-    IERC20 public immutable asset; // Underlying assets (such as USDT)
+    IERC20 public immutable ASSET; // Underlying assets (such as USDT)
 
     // 2. constructor
     // _asset: what token we want to deposit (USDT address)
     // "Vault USDT", "vUSDT": the name of the token that users receive
     constructor(IERC20 _asset) ERC20("Vault USDT", "vUSDT") {
-        asset = _asset;
+        ASSET = _asset;
     }
 
     // ==========================================
@@ -35,12 +35,12 @@ contract Vault is ERC20 {
             shares = (assets * totalSupply()) / totalAssets();
         }
 
-        // B. First, transfer the user's money in (Checks-Effects Interactions mode: 
+        // B. First, transfer the user's money in (Checks-Effects Interactions mode:
         // change the status first and then transfer, this is slightly special because mint is at the end)
         // Attention: Users must first approve this contract!
         // The return value of transferFrom needs to be checked (SafeERC20 is omitted here for code simplicity,
         // and must be used in production environments)
-        bool success = asset.transferFrom(msg.sender, address(this), assets);
+        bool success = ASSET.transferFrom(msg.sender, address(this), assets);
         require(success, "Transfer failed");
 
         //C. Casting vouchers for users
@@ -67,15 +67,15 @@ contract Vault is ERC20 {
         _burn(msg.sender, shares);
 
         // C. Transfer the money to the user
-        bool success = asset.transfer(msg.sender, payoutAmount);
+        bool success = ASSET.transfer(msg.sender, payoutAmount);
         require(success, "Transfer failed");
 
         // D. (Optional) Throw an event
         // emit Withdraw(msg.sender, payoutAmount, shares);
     }
-    
+
     // How many are currently in the vault USDT
     function totalAssets() public view returns (uint256) {
-        return asset.balanceOf(address(this));
+        return ASSET.balanceOf(address(this));
     }
 }
