e9ade8819e08ce8261e26344eb9004ef26f97b18
GuangchaoYi <1241354012@qq.com>
2026-02-16 00:00:22 +0800

    feat: #72 optimize code

--- script/DeployReward.s.sol
+++ script/DeployReward.s.sol
@@ -7,8 +7,7 @@ import {MerkleDistributor} from "../src/MerkleDistributor.sol";
 
 contract DeployReward is Script {
     function run() external returns (address rwdAddr, address distributorAddr) {
-        uint256 deployerPrivateKey =
-            0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+        uint256 deployerPrivateKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
         vm.startBroadcast(deployerPrivateKey);
 
